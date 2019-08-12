b340dd61b319c0b5bd1c3e1e6d25d9cea1c9e9ae
march <francois.cousin62880@gmail.com>
2019-08-12 13:40:37 +0200

    1er

---
+++ script.js
@@ -0,0 +1,33 @@
+/************************* Compétences ****************************************/
+
+// bar de progression
+
+$(document).ready(function() {
+	$('.progress .progress-bar').css("width",
+			  function() {
+				  return $(this).attr("aria-valuenow") + "%";
+			  }
+	  )
+  });
+/************************* Hobbies ****************************************/
+
+// 3D Coverflow Effect
+
+  var swiper = new Swiper('.swiper-container', {
+	effect: 'coverflow',
+	grabCursor: true,
+	centeredSlides: true,
+	slidesPerView: 'auto',
+	coverflowEffect: {
+	  rotate: 30,
+	  stretch: 0,
+	  depth: 200,
+	  modifier: 1,
+	  slideShadows : true,
+	},
+	pagination: {
+	  el: '.swiper-pagination',
+	},
+  });
+
+ 
